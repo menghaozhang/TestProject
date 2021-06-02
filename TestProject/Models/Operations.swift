bf50c66743aab3d430376ca5db9053e22d7bfa4a
Menghao Zhang <menghao.zhang@tink.se>
2021-06-02 11:01:59 +0200

    Update operations to enum

--- TestProject/Models/Operations.swift
+++ TestProject/Models/Operations.swift
@@ -7,19 +7,11 @@
 
 import Foundation
 
-struct Operations: OptionSet {
-    let rawValue: Int
-
-    static let plus = Operations(rawValue: 1 << 0)
-    static let minus = Operations(rawValue: 1 << 1)
-    static let times = Operations(rawValue: 1 << 2)
-    static let divide = Operations(rawValue: 1 << 3)
-    static let sin = Operations(rawValue: 1 << 4)
-    static let cos = Operations(rawValue: 1 << 5)
-
-    static let all: Operations = [.plus, .minus, .times, .divide, .sin, .cos]
-
-    init(rawValue: Int) {
-        self.rawValue = rawValue
-    }
+enum Operation: String {
+    case plus = "+"
+    case minus = "-"
+    case times = "*"
+    case divide = "/"
+    case sin
+    case cos
 }
