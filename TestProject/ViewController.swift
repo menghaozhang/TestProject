69dfefc95789f51c4472a666d5299e938f571c08
Menghao Zhang <menghao.zhang@tink.se>
2021-06-02 13:56:35 +0200

    layout the views

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var stackView = UIStackView()
+    private var operandsView = OperandsView()
+    private var operationsView = OperationsView()
+    private let outputLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemBackground
+
+        view.addSubview(outputLabel)
+        view.addSubview(stackView)
+
+        stackView.distribution = .fillEqually
+        stackView.addArrangedSubview(operandsView)
+        stackView.addArrangedSubview(operationsView)
+
+        outputLabel.font = UIFont.boldSystemFont(ofSize: 40)
+        outputLabel.textAlignment = .right
+
+        outputLabel.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            outputLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            outputLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            outputLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            outputLabel.heightAnchor.constraint(equalToConstant: 80),
+
+            stackView.topAnchor.constraint(equalTo: outputLabel.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
     }
-
-
 }
 
