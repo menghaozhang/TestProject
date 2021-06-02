c7efd819a1916f5b5a0debeeae5559ae141e3bea
Menghao Zhang <menghao.zhang@tink.se>
2021-06-02 07:59:27 +0200

    Initial project

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Menghao Zhang on 2021-06-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
